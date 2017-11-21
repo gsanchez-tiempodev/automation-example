da63d5bbe5b1945f34f49228714b0f96b7a93c31
gsanchez-tiempodev <gsanchez@tiempodev.com>
2017-11-21 10:58:45 -0600

    changed xpath Our Solution

--- lib/hubbub/automation/welcome_page.rb
+++ lib/hubbub/automation/welcome_page.rb
@@ -1,6 +1,6 @@
 class WelcomePage < Capybara::PageMapper::Base
   define_button :why_hubbub, '//a[@title="Why hubbub"]'
-  define_button :our_solution, '/html/body/div/nav/ul/li[2]/a'
+  define_button :our_solution, './/li[2]/a[text()="Our Solution"]'
 
   def why_hubbub!
     why_hubbub_button.trigger('click')
